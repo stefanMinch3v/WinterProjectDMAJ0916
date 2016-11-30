728f3ab4919361dbd8a3f13a0b92322f707410c3
stefan <amxx@abv.bg>
2016-11-30 11:17:32 +0100

    last changes

--- Source/ModelLayer/ContractorContainer.java
+++ Source/ModelLayer/ContractorContainer.java
@@ -3,7 +3,7 @@ package ModelLayer;
 import java.util.ArrayList;
 
 /**
- * Created by RedJohn on 23-Nov-16. porn
+ * Created by RedJohn on 23-Nov-16.
  */
 public class ContractorContainer {
     private  ArrayList<Contractor> contractors;
